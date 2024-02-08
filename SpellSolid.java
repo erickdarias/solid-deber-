55f2ba3b9863c0d21914c855bf4d851dd3fb67c9
erickdarias <ariaserick580@gmail.com>
2024-02-08 17:05:55 -0500

    deber solid

---
+++ SpellSolid.java
@@ -0,0 +1,10 @@
+public class SpellSolid {
+    public static void main(String[] args) {
+        String word = "solid";
+        
+        // Iterar sobre cada letra de la palabra y deletrearla
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
+    }
+}
